7926286faf621c0c218463810b000ffe4a7f399e
Storm-Engineering <armystorms@gmail.com>
2019-07-01 09:46:09 -0800

    Update config
    Seeing if it will only test the circleci branch.

--- tests/test-registration.php
+++ tests/test-registration.php
@@ -8,12 +8,12 @@
 /**
  * Sample test case.
  */
-class SampleTest extends WP_UnitTestCase {
+class RegistrationTest extends WP_UnitTestCase {
 
 	/**
 	 * A single example test.
 	 */
-	public function test_sample() {
+	public function test_registrations() {
 		// Replace this with some actual testing code.
 		$this->assertTrue( true );
 	}
