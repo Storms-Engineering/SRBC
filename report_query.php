8e68285b5698eb80033d87cae36cf8515e2af9ea
Storm-Engineering <armystorms@gmail.com>
2019-07-19 11:22:40 -0800

    Security change
    Allows program to see this report.  Will be fixing

--- report_query.php
+++ report_query.php
@@ -2,7 +2,7 @@
 //Import $wpdb for wordpress
 require($_SERVER['DOCUMENT_ROOT'].'/wp-load.php');
 //Custom Security Check
-if (!is_user_logged_in() && !isset($_GET["camp_numbers"])) exit("Thus I refute thee.... P.H.");
+if (!is_user_logged_in() && $_GET["report"] !== "camp_numbers") exit("Thus I refute thee.... P.H.");
 
 require 'requires/reports.php';
 
