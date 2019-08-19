ab8d88312bd94fcfbcf6c14b4737312960b8ba3e
Storm-Engineering <armystorms@gmail.com>
2019-08-19 15:48:04 -0800

    Update email todo

--- functions.php
+++ functions.php
@@ -1,7 +1,8 @@
 <?php
 //Collection of various random functions that come in handy
 define('srbc_email', 'info@solidrockbiblecamp.com');
-define('workcrew_email', 'armystorms@gmail.com');
+//TODO Update Email address to WorkCrew manager
+define('workcrew_email', 'info@solidrockbiblecamp');
 
 $GLOBALS['srbc_camps'] = "srbc_camps" . get_option("srbc_database_year");
 $GLOBALS['srbc_payments'] = "srbc_payments" . get_option("srbc_database_year");
