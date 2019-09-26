bbe18837c4cdda30e053c3f406fad88002114b73
Storms-Engineering <armystorms@gmail.com>
2019-09-26 09:52:48 -0800

    Updated files for testing
    Also changed email to __dir__ because phpunit likes that better

--- requires/email.php
+++ requires/email.php
@@ -1,8 +1,8 @@
 <?php
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
-require_once $_SERVER['DOCUMENT_ROOT'] . '/wp-content/plugins/SRBC/PHPMailer/src/Exception.php';
-require_once $_SERVER['DOCUMENT_ROOT'] . '/wp-content/plugins/SRBC/PHPMailer/src/PHPMailer.php';
+require_once __DIR__ . '/../PHPMailer/src/Exception.php';
+require_once __DIR__ . '/../PHPMailer/src/PHPMailer.php';
 
 class Email
 {
