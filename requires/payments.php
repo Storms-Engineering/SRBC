13892d8c48ce705a29b4dabc81a1566675599fd8
Storms-Engineering <armystorms@gmail.com>
2020-05-01 10:50:00 -0800

    Created Payments class
    Updated updated_registration to use new class.

---
+++ requires/payments.php
@@ -0,0 +1,142 @@
+<?php
+class Payments
+{
+    //Puts a payment into the database and also updates payment_card payment_cash etc...
+    public static function makePayment($registration_id,$payment_type,$payment_amt,$note,$fee_type)
+    {
+        global $wpdb;
+        //Get the current date time
+        $current_user = wp_get_current_user();
+        $username = $current_user->user_login;
+        $is_registration = 0;
+        if (strpos($username, 'registration') !== false)
+            $is_registration = 1;
+
+        $date = new DateTime("now", new DateTimeZone('America/Anchorage'));
+        global $wpdb;
+        $wpdb->insert(
+                $GLOBALS['srbc_payments'], 
+                array( 
+                    'payment_id' =>0,
+                    'registration_id' => $registration_id,
+                    'payment_type' => $payment_type,
+                    'payment_amt' => $payment_amt,
+                    'payment_date' =>  $date->format("m/d/Y G:i"),
+                    'note' => $note ,
+                    'fee_type' => $fee_type,
+                    'registration_day' => $is_registration,
+                    'entered_by' => $current_user->display_name
+                ), 
+                array( 
+                    '%d',
+                    '%d', 
+                    '%s',
+                    '%f',
+                    '%s',
+                    '%s',
+                    '%s',
+                    '%d',
+                    '%s'				
+                ) 
+            );
+    }
+
+    //Calculates how much they need to pay and makes the payment
+    public static function calculatePaymentAmt($autoPaymentAmt, $needToPayAmount)
+    {
+        $paymentAmt = 0;
+        
+        if ($autoPaymentAmt <= $needToPayAmount)
+            $paymentAmt = $autoPaymentAmt;
+        else if($autoPaymentAmt > $needToPayAmount)
+            $paymentAmt = $needToPayAmount;
+        //this is how much money is left so subtract what we just paid
+        $autoPaymentAmt -= $paymentAmt;
+        return array($autoPaymentAmt,$paymentAmt);
+    }
+
+    public static function autoPayment($registration_id,$autoPaymentAmt,$paymentType,$note)
+    {
+        global $wpdb;
+        $o = $wpdb->get_row( $wpdb->prepare("SELECT * FROM " . $GLOBALS['srbc_registration'] . " WHERE registration_id=%d ",$registration_id));
+		$totalpaid = $wpdb->get_var($wpdb->prepare("SELECT SUM(payment_amt) 
+								FROM " . $GLOBALS['srbc_payments'] . " WHERE registration_id=%s AND NOT fee_type='Store'",$registration_id));
+		
+		//Make the scholarships and discounts add to total paid so we take it out of the base camp fee
+		$totalpaid += $o->discount + $o->scholarship_amt;
+		if($totalpaid == NULL)
+			$totalpaid = 0;
+		//Check if they have paid the base camp amount which is (camp cost - horse cost)
+		$camp = $wpdb->get_row("SELECT * FROM " . $GLOBALS['srbc_camps'] ." WHERE camp_id=$o->camp_id");
+		$baseCampCost = $camp->cost - $camp->horse_cost;
+		$needToPayAmount = 0;
+		$feeType = NULL;
+		//Counts how many times we looped through
+		$loops = 0;
+		//Calculate bus fee based on type of busride
+		$busfee = 0;
+		if ($o->busride == "both")
+			$busfee = 60;
+		else if($o->busride == "to" || $o->busride == "from")
+			$busfee = 35;
+		
+		$horseOpt = 0;
+		if ($o->horse_opt == 1)
+			$horseOpt = $camp->horse_opt_cost;
+		//Create seperate payments based on different fees until autoPaymentAmt is used up
+		//or an overpayment happens which stores it in the database
+		while ($autoPaymentAmt != 0)
+		{
+			if ($totalpaid < $baseCampCost)
+			{
+				//We still need to pay some on the base camp cost
+				$needToPayAmount = $baseCampCost - $totalpaid;
+				if ($camp->area == "Sports")
+					$feeType = "Lakeside";
+				else
+					$feeType = $camp->area;
+			}				
+			//$totalpaid comes first because this also checks that they have paid more than we are currently looking atan
+			//If we flip it then it becomes a negative number if the totalpaid is greater than the value we are checking
+			//Check horse_cost (aka WT Horsemanship Fee
+			else if(($totalpaid - $baseCampCost) < $camp->horse_cost) 
+			{
+				//We still need to pay some on the base camp cost
+				$needToPayAmount = $camp->horse_cost - ($baseCampCost - $totalpaid);
+				$feeType = "WT Horsemanship";
+			}				
+			//Horse option check aka LS Horsemanship
+			else if(($totalpaid - $camp->cost) < $horseOpt) 
+			{
+				//We still need to pay some on the horse option
+				$needToPayAmount = $horseOpt - ($totalpaid - $camp->cost);
+				$feeType = "LS Horsemanship";
+			}
+			else if(($totalpaid - ($camp->cost + $horseOpt)) < $busfee) 
+			{
+				//We still need to pay some on the bus option
+				$needToPayAmount = $busfee - ($totalpaid - ($camp->cost + $horseOpt));
+				$feeType = "Bus";
+			}
+			else
+			{
+				//Overpaid
+				$needToPayAmount = $autoPaymentAmt;
+				$feeType= "Overpaid";
+			}
+			//Also updates autoPaymentAmt
+			list ($autoPaymentAmt,$paid) = self::calculatePaymentAmt($autoPaymentAmt,$needToPayAmount);
+			self::makePayment($registration_id,$paymentType,$paid,
+				$note,$feeType);
+			$totalpaid += $paid;
+			$loops++;
+			if ($loops > 10)
+			{
+				error_msg("Error: Autopayment failed!  Infinite loop detected.... Please let Website administrator know. - Peter H.");
+				break;
+				
+			}
+		}
+    }
+}
+?>
--- update_registration.php
+++ update_registration.php
@@ -8,6 +8,9 @@ require($_SERVER['DOCUMENT_ROOT'].'/wp-load.php');
 if (!is_user_logged_in()) exit("Thus I refute thee.... P.H.");
 global $wpdb;
 
+//Pull in our payments class
+require_once __DIR__ . '/requires/payments.php';
+
 
 if (isset($obj["delete_payment_id"]))
 {
@@ -21,7 +24,7 @@ if (isset($obj["delete_payment_id"]))
 }
 else if (isset($obj["reactivate_id"]))
 {
-	//Delete the requested registration
+	//Move inactive registration to active registration database
 	$query = $wpdb->query($wpdb->prepare("INSERT INTO " . $GLOBALS['srbc_registration'] . " SELECT * FROM " . $GLOBALS['srbc_registration_inactive']. 
 	" WHERE registration_id=%d;",$obj["reactivate_id"]));
 	if($query === 0 || $query === false)
@@ -195,151 +198,23 @@ else {
 		);
 		if ($obj[$key]["snackshop"] != "")
 		{
-			makePayment($key,$obj[$key]['snackshop_payment_type'],$obj[$key]["snackshop"],
+			Payments::makePayment($key,$obj[$key]['snackshop_payment_type'],$obj[$key]["snackshop"],
 				"","Store");
 		}
 		if($obj[$key]["auto_payment_amt"] != "")
 		{
-			$o = $wpdb->get_row( $wpdb->prepare("SELECT * FROM " . $GLOBALS['srbc_registration'] . " WHERE registration_id=%d ",$key));
-			$totalpaid = $wpdb->get_var($wpdb->prepare("SELECT SUM(payment_amt) 
-									FROM " . $GLOBALS['srbc_payments'] . " WHERE registration_id=%s AND NOT fee_type='Store'",$key));
-			
-			//Make the scholarships and discounts add to total paid so we take it out of the base camp fee
-			$totalpaid += $o->discount + $o->scholarship_amt;
-			if($totalpaid == NULL)
-				$totalpaid = 0;
-			//Check if they have paid the base camp amount which is (camp cost - horse cost)
-			$camp = $wpdb->get_row("SELECT * FROM " . $GLOBALS['srbc_camps'] ." WHERE camp_id=$o->camp_id");
-			$baseCampCost = $camp->cost - $camp->horse_cost;
-			$needToPayAmount = 0;
-			$feeType = NULL;
-			//Counts how many times we looped through
-			$loops = 0;
-			$autoPaymentAmt = $obj[$key]["auto_payment_amt"];
-			//Calculate bus fee based on type of busride
-			$busfee = 0;
-			if ($o->busride == "both")
-				$busfee = 60;
-			else if($o->busride == "to" || $o->busride == "from")
-				$busfee = 35;
-			
-			$horseOpt = 0;
-			if ($o->horse_opt == 1)
-				$horseOpt = $camp->horse_opt_cost;
-			//Create seperate payments based on different fees until autoPaymentAmt is used up
-			//or an overpayment happens which stores it in the database
-			while ($autoPaymentAmt != 0)
-			{
-				if ($totalpaid < $baseCampCost)
-				{
-					//We still need to pay some on the base camp cost
-					$needToPayAmount = $baseCampCost - $totalpaid;
-					if ($camp->area == "Sports")
-						$feeType = "Lakeside";
-					else
-						$feeType = $camp->area;
-				}				
-				//$totalpaid comes first because this also checks that they have paid more than we are currently looking atan
-				//If we flip it then it becomes a negative number if the totalpaid is greater than the value we are checking
-				//Check horse_cost (aka WT Horsemanship Fee
-				else if(($totalpaid - $baseCampCost) < $camp->horse_cost) 
-				{
-					//We still need to pay some on the base camp cost
-					$needToPayAmount = $camp->horse_cost - ($baseCampCost - $totalpaid);
-					$feeType = "WT Horsemanship";
-				}				
-				//Horse option check aka LS Horsemanship
-				else if(($totalpaid - $camp->cost) < $horseOpt) 
-				{
-					//We still need to pay some on the horse option
-					$needToPayAmount = $horseOpt - ($totalpaid - $camp->cost);
-					$feeType = "LS Horsemanship";
-				}
-				else if(($totalpaid - ($camp->cost + $horseOpt)) < $busfee) 
-				{
-					//We still need to pay some on the bus option
-					$needToPayAmount = $busfee - ($totalpaid - ($camp->cost + $horseOpt));
-					$feeType = "Bus";
-				}
-				else
-				{
-					//Overpaid
-					$needToPayAmount = $autoPaymentAmt;
-					$feeType= "Overpaid";
-				}
-				//Also updates autoPaymentAmt
-				list ($autoPaymentAmt,$paid) = calculatePaymentAmt($autoPaymentAmt,$needToPayAmount);
-				makePayment($key,$obj[$key]["auto_payment_type"],$paid,
-					$obj[$key]["auto_note"],$feeType);
-				$totalpaid += $paid;
-				$loops++;
-				if ($loops > 5)
-				{
-					error_msg("Error: Autopayment failed!  Infinite loop detected.... Please let Website administrator know. - Peter H.");
-					break;
-					
-				}
-			}
-			
+			Payments::autoPayment($key,$obj[$key]["auto_payment_amt"],$obj[$key]["auto_payment_type"],
+									$obj[$key]["auto_note"]);
 		}
 		else if ($obj[$key]["payment_type"] != "none"){
-			makePayment($key,$obj[$key]["payment_type"],$obj[$key]["payment_amt"],
+			Payments::makePayment($key,$obj[$key]["payment_type"],$obj[$key]["payment_amt"],
 				$obj[$key]["note"],$obj[$key]["fee_type"]);
 		}
 		
 	}
 	echo "Data Saved Sucessfully";
 }
-//Calculates how much they need to pay and makes the payment
-function calculatePaymentAmt($autoPaymentAmt, $needToPayAmount)
-{
-	$paymentAmt = 0;
-	
-	if ($autoPaymentAmt <= $needToPayAmount)
-		$paymentAmt = $autoPaymentAmt;
-	else if($autoPaymentAmt > $needToPayAmount)
-		$paymentAmt = $needToPayAmount;
-	//this is how much money is left so subtract what we just paid
-	$autoPaymentAmt -= $paymentAmt;
-	return array($autoPaymentAmt,$paymentAmt);
-}
 
-//Puts a payment into the database and also updates payment_card payment_cash etc...
-function makePayment($registration_id,$payment_type,$payment_amt,$note,$fee_type)
-{
-	//Get the current date time
-	$current_user = wp_get_current_user();
-	$username = $current_user->user_login;
-	$is_registration = 0;
-	if (strpos($username, 'registration') !== false)
-		$is_registration = 1;
 
-	$date = new DateTime("now", new DateTimeZone('America/Anchorage'));
-	global $wpdb;
-	$wpdb->insert(
-			$GLOBALS['srbc_payments'], 
-			array( 
-				'payment_id' =>0,
-				'registration_id' => $registration_id,
-				'payment_type' => $payment_type,
-				'payment_amt' => $payment_amt,
-				'payment_date' =>  $date->format("m/d/Y G:i"),
-				'note' => $note ,
-				'fee_type' => $fee_type,
-				'registration_day' => $is_registration,
-				'entered_by' => $current_user->display_name
-			), 
-			array( 
-				'%d',
-				'%d', 
-				'%s',
-				'%f',
-				'%s',
-				'%s',
-				'%s',
-				'%d',
-				'%s'				
-			) 
-		);
-}
+
 ?>
